61b797f14b22458d04949ddda67e435ff91941d2
Sadie Pretzlaff <sadiepretzlaff@gmail.com>
2025-09-02 15:42:11 -0600

    Started program in class, implemented GetValue() and started GetRange()

--- Program.cs
+++ Program.cs
@@ -10,6 +10,30 @@ namespace ICA01_SadieP
     {
         static void Main(string[] args)
         {
+            GetRange(out int min, out int max);
+        }
+        static void GetRange(out int min, out int max)
+        {
+            Console.WriteLine("Input the minimum value.");
+            min = GetValue(1, 2);
+            max = GetValue(1, 2);
+        }
+        static int GetValue(int min, int max)
+        {
+            bool success = false;
+            int value = 0;
+            do
+            {
+                Console.Write($"Please input an int between {min} and {max}: ");
+                success = int.TryParse(Console.ReadLine(), out value);
+                if (!success)
+                {
+                    Console.WriteLine("Error: Value invalid. Please try again.\n");
+                }
+            } while (!success);
+
+
+            return value;
         }
     }
 }
